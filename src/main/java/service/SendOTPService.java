ae8040a7a60c7472f74e9f730ede2b6dc27ef1ae
riyashinde203 <93462095+riyashinde203@users.noreply.github.com>
2024-07-10 18:06:02 +0530

    FileHider commit

---
+++ src/main/java/service/SendOTPService.java
@@ -0,0 +1,65 @@
+package service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendOTPService {
+    public static void sendOTP(String email, String genOTP) {
+        // Recipient's email ID
+        String to = email;
+
+        // Sender's email ID
+        String from = "riyashinde173@gmail.com";
+
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(from, "crtv smfi dzsg rsle");
+            }
+
+        });
+
+        session.setDebug(true);
+
+        try {
+
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("File Enc OTP");
+
+            // actual message
+            message.setText("Your One time Password for File Enc app is " + genOTP);
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+    }
+
+}
